721cbbb1fa51aa936a4fff09fe6c0623aa4d6d8e
Stas <stasprokopyshyn@gmail.com>
2021-06-30 22:07:54 +0300

    task is well done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let specialCharacters = ['.', '+', '*', '?', '^', '$', '(', ')', '[', ']', '{', '}', '|', '\\'];
+  let brackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let regexPart = '';
+    regexPart += specialCharacters.includes(bracketsConfig[i][0]) ? `\\${bracketsConfig[i][0]}` : `${bracketsConfig[i][0]}`;
+    regexPart += specialCharacters.includes(bracketsConfig[i][1]) ? `\\${bracketsConfig[i][1]}` : `${bracketsConfig[i][1]}`;
+    brackets.push(regexPart);
+  }
+
+  brackets = brackets.join('|');
+
+  let regex = new RegExp(brackets);
+  console.log(regex);
+
+  while (true) {
+    if (!str.match(regex)) {
+      
+      break;
+    }
+
+    str = str.replace(regex, '');
+  }
+  console.log(str.length);
+
+  return str.length == 0 ? true : false;
+ 
+};
